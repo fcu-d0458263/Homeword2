3a662aef01e0bc2a0a2e9ff98508e2861c123fa8
user <user@FF03>
2016-10-07 09:27:37 +0800

    complete

---
+++ src/PrintNumberInWord.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PrintNumberInWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] str={"one","two","three","four","five","six","seven","eight","nine"};
+		int num;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a integer: ");
+		num = scanner.nextInt();
+		if(num <1 || num>9)
+		{
+			System.out.print("OTHER");
+		}
+		else
+		{
+			System.out.printf("%s",str[num-1]);
+		}
+	}
+
+}
---
+++ src/StringComparator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class StringComparator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1,str2;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a string 1:");
+		str1 = scanner.next();
+		System.out.print("Enter a string 2:");
+		str2 = scanner.next();
+		
+		if(str1.equalsIgnoreCase(str2))
+		{
+			System.out.print("The two strings are the same.");
+		}
+		else
+		{
+			System.out.print("The two strings are not the same.");
+		}
+	}
+
+}
