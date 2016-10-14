d5385266a7788040d41d966bedc16ec61153129d
user <user@FH04>
2016-10-14 09:43:07 +0800

    hw3

---
+++ src/Continue.java
@@ -0,0 +1,13 @@
+
+public class Continue {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i ++) {
+			
+			if(i!=3 && i!=1)
+			{
+				continue ;
+			}
+		System.out.println(i);
+		}
+	}
+}
--- src/StringComparator.java
+++ src/StringComparator.java
@@ -4,7 +4,8 @@ public class StringComparator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str1,str2;
-		Scanner scanner = new Scanner(System.in);
+		while(true){
+	    Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a string 1:");
 		str1 = scanner.next();
 		System.out.print("Enter a string 2:");
@@ -13,12 +14,14 @@ public class StringComparator {
 		if(str1.equalsIgnoreCase(str2))
 		{
 			System.out.print("The two strings are the same.");
+			scanner.close();
+			break;
 		}
 		else
 		{
-			System.out.print("The two strings are not the same.");
+			continue ;
 		}
-		scanner.close();
+		}
 	}
 
-}
+}  
